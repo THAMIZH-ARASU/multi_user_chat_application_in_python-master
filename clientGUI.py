ae93295baf0567bc9a2943b7a4348b32e363c15f
THAMIZH-ARASU <113611809+THAMIZH-ARASU@users.noreply.github.com>
2022-12-20 20:53:08 +0530

    Updated client
    The clientGUI.py file is updated with new interface and design

---
+++ clientGUI.py
@@ -0,0 +1,130 @@
+import tkinter as tk
+from tkinter import messagebox
+import socket
+import threading
+
+window = tk.Tk()
+window.configure(background = "black")
+window.title("Client")
+username = " "
+
+label1 = tk.Label(window, text = "CLIENT", fg = "green", bg = "black", font = ("TimesNewRoman",18))
+label1.pack(side = tk.TOP)
+topFrame = tk.Frame(window, background = "black")
+lblName = tk.Label(topFrame, text = "Name:", background = "black", fg = "blue", font = ("TimesNewRoman", 10)).pack(side=tk.LEFT)
+entName = tk.Entry(topFrame, bg = "white", fg = "black")
+entName.pack(side=tk.LEFT)
+btnConnect = tk.Button(topFrame, text="Connect", fg = "green", bg = "black", font = ("TimesNewRoman", 8), command=lambda : connect())
+btnConnect.pack(side=tk.LEFT, padx=(5,0))
+#btnConnect.bind('<Button-1>', connect)
+topFrame.pack(side=tk.TOP)
+
+displayFrame = tk.Frame(window, background = "black")
+lblLine = tk.Label(displayFrame, text="--------------------------------------------------------------------------------------", fg = "white", bg = "black").pack()
+scrollBar = tk.Scrollbar(displayFrame)
+scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+tkDisplay = tk.Text(displayFrame, height=20, width=55, fg = "white", bg = "black")
+tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
+tkDisplay.tag_config("tag_your_message", foreground="blue")
+scrollBar.config(command=tkDisplay.yview)
+tkDisplay.config(yscrollcommand=scrollBar.set, state="disabled")
+displayFrame.pack(side=tk.TOP)
+
+
+bottomFrame = tk.Frame(window, background = "black")
+tkMessage = tk.Text(bottomFrame, height=2, width=55, fg = "white", bg = "black")
+tkMessage.pack(side=tk.LEFT, padx=(5, 13), pady=(5, 10))
+tkMessage.config(highlightbackground="grey", state="disabled")
+tkMessage.bind("<Return>", (lambda event: getChatMessage(tkMessage.get("1.0", tk.END))))
+bottomFrame.pack(side=tk.BOTTOM)
+
+
+def connect():
+    global username, client
+    if len(entName.get()) < 1:
+        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
+    else:
+        username = entName.get()
+        connect_to_server(username)
+
+
+# network client
+client = None
+HOST_ADDR = "127.0.0.1"
+HOST_PORT = 1025
+
+def connect_to_server(name):
+    global client, HOST_PORT, HOST_ADDR
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST_ADDR, HOST_PORT))
+        client.send(name.encode()) # Send name to server after connecting
+
+        entName.config(state=tk.DISABLED)
+        btnConnect.config(state=tk.DISABLED)
+        tkMessage.config(state=tk.NORMAL)
+
+        # start a thread to keep receiving message from server
+        # do not block the main thread :)
+        threading._start_new_thread(receive_message_from_server, (client, "m"))
+    except Exception as e:
+        tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
+
+
+def receive_message_from_server(sck, m):
+    while True:
+        from_server = sck.recv(4096).decode()
+
+        if not from_server: break
+
+        # display message from server on the chat window
+
+        # enable the display area and insert the text and then disable.
+        # why? Apparently, tkinter does not allow us insert into a disabled Text widget :(
+        texts = tkDisplay.get("1.0", tk.END).strip()
+        tkDisplay.config(state=tk.NORMAL)
+        if len(texts) < 1:
+            tkDisplay.insert(tk.END, from_server)
+        else:
+            tkDisplay.insert(tk.END, "\n\n"+ from_server)
+
+        tkDisplay.config(state=tk.DISABLED)
+        tkDisplay.see(tk.END)
+
+        # print("Server says: " +from_server)
+
+    sck.close()
+    window.destroy()
+
+
+def getChatMessage(msg):
+
+    msg = msg.replace('\n', '')
+    texts = tkDisplay.get("1.0", tk.END).strip()
+
+    # enable the display area and insert the text and then disable.
+    # why? Apparently, tkinter does not allow use insert into a disabled Text widget :(
+    tkDisplay.config(state=tk.NORMAL)
+    if len(texts) < 1:
+        tkDisplay.insert(tk.END, "You->" + msg, "tag_your_message") # no line
+    else:
+        tkDisplay.insert(tk.END, "\n\n" + "You->" + msg, "tag_your_message")
+
+    tkDisplay.config(state=tk.DISABLED)
+
+    send_mssage_to_server(msg)
+
+    tkDisplay.see(tk.END)
+    tkMessage.delete('1.0', tk.END)
+
+
+def send_mssage_to_server(msg):
+    client_msg = str(msg)
+    client.send(client_msg.encode())
+    if msg == "exit":
+        client.close()
+        window.destroy()
+    print("Sending message")
+
+
+window.mainloop()
